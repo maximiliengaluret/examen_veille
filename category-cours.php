8a7f1e7d317ca8fb1e930a4048e2a8de4a21ec99
Galuret <e1626732@cmaisonneuve.qc.ca>
2020-03-10 16:54:22 -0400

    Question 3

--- category-cours.php
+++ category-cours.php
@@ -19,20 +19,26 @@ get_header();
        
 			<header class="page-header">
 
-            <h1>Question 2</h1>
+            <h1>Question 3</h1>
  
                 <?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
-                   
+                echo '<ol>';
                  // The Loop
                  while (have_posts() ) {
                  the_post();
-                 echo '<h4>' . get_the_title() . '</h4>';
-          
- 
+               
+                 echo '<li>';
+                 echo '<a href=http://localhost/2020-maximilien/2011/11/01/tim-'. substr(get_the_title(),4,7) . '>' . get_the_title().' - <span class="sess">session :</span>'. substr(get_the_title(),4,1)  .' - <span class=dom>domaine :</span>'. substr(get_the_title(),5,1) .  '</a>';
+                 echo '</li>';
+                
+                
+             
+
+
                 }
- 
+                echo '</ol>';
                 ?>
                 
                 
